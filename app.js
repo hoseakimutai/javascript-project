12c12b17bbb49fadb0b2e961b9c06e1a549edf0c
Hosea Cheboi <kimutaihosea98@gmail.com>
2023-04-21 12:21:16 +0300

    my second commit

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
-// let num = 1;
-// while(num<=100){
+// document.getElementById("myBtn").addEventListener("click", displaygrade);
 
-//     // Print each number
-//     console.log(num);
-
-//     // Increase num by 1 
-//     num = num + 1;
-// }
-for (i = 1; i < 100; i++) {
-    if (i % 3 == 0) {
-        console.log("Fizz");
-    } else if (i % 5) {
-        console.log("Buzz");
-    } else {
-        console.log("FizzBuzz")
-    }
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
 }
-
-
-
-
-
-// for (var i = 1; i <= 100; i++) {
-//     if (i % 3 == 0 && i % 5 == 0) console.log("FizzBuzz");
-//     else if (i % 3 == 0) console.log("Fizz");
-//     else if (i % 5 == 0) console.log("Buzz");
-//     else console.log(i);
-// }
 
 
 
@@ -46,97 +25,36 @@ console.log(count);
 
 
 
-
 function myFunction() {
-    switch (prompt("what is your grade")) {
-      case "90>":
-        console.log(A)
-        break;
-      case "85-90":
-        console.log(`A-`)
-        break;
-      case "80-84":
-        console.log(`B+`)
-        break;
-      case "75-79":
-        console.log(`B`)
-        break;
-      case "70-74":
-        console.log(`B-`)
-        break;
-      case "60-69":
-        console.log(`C+`)
-        break;
-      case "50-59":
-        console.log(`C`)
-        break;
-      case "<50":
-        console.log(`fail`)
-        break;
-    }
+  let score = prompt("enter your grade")
+
+  switch (true) {
+    case (score > 90 && score <= 100):
+      console.log("A")
+      break;
+    case (score >= 85 && score <= 90):
+      console.log("A-")
+      break;
+    case (score >= 80 && score <= 84):
+      console.log("B+")
+      break;
+    case (score >= 75 && score <= 79):
+      console.log("B")
+      break;
+    case (score >= 70 && score <= 74):
+      console.log("B-")
+      break;
+    case (score >= 60 && score <= 69):
+      console.log("C+")
+      break;
+    case (score >= 50 && score <= 59):
+      console.log("C")
+      break;
+    case (score <= 50):
+      console.log("fail")
+      break;
   }
+}
 
+myFunction()
 
-
-
-// function myFunction() {
-//     switch (prompt("what is your grade")) {
-//         case "90>":
-//             result=("A")
-//             break;
-
-//             case "85-90":
-//             result=('A-')
-//             break;
-
-//             case "80-84":
-//             result=('B+')
-//             break;
-
-//             case "75-79":
-//             result=('B')
-//             break;
-
-//             case "70-74":
-//             result=('B-')
-//             break;
-
-//             case "60-69":
-//             result=('C+')
-//             break;
-
-//             case "50-59":
-//             result=('C')
-//             break;
-
-//             case "<50":
-//             result=('fail')
-//             break;
-//     }
-// }
-// }
-// console.log(result);
-// // function myFunction(){
-// // switch (new Date().getDay()) {
-// //     case 0:
-// //       day = "Sunday";
-// //       break;
-// //     case 1:
-// //       day = "Monday";
-// //       break;
-// //     case 2:
-// //        day = "Tuesday";
-// //       break;
-// //     case 3:
-// //       day = "Wednesday";
-// //       break;
-// //     case 4:
-// //       day = "Thursday";
-// //       break;
-// //     case 5:
-// //       day = "Friday";
-// //       break;
-// //     case 6:
-// //       day = "Saturday";
-// //   }
-// // }  
